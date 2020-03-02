b5e0de511593c9ebdec28438e0bfb756cee4eb6c
lijunwei <lijunwei@udesk.cn>
2020-03-02 10:01:11 +0800

    创建了create动作

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,17 @@
 class ArticlesController < ApplicationController
+
+  # skip_before_action :verify_authenticity_token, :only => [:create, :new]
+
+
   def new
   end
+
+
+
+  def create
+
+    render plain: params[:article].inspect
+    # render json: params
+  end
+
 end
