3927de6f8e8a19de01a35cd415aaebfa7422c803
lijunwei <lijunwei@udesk.cn>
2020-03-02 15:23:45 +0800

    在article controller中使用logger打印了一些信息

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,21 +5,29 @@ class ArticlesController < ApplicationController
   # skip_before_action :verify_authenticity_token, :only => [:create, :new]
 
   def index
+    logger.info "---> entering [#{__method__}] action of article controller..."
     @articles = Article.all
   end
 
   def new
+    logger.info "---> entering [#{__method__}] action of article controller..."
     @article = Article.new
   end
 
   def edit
+    logger.info "---> entering [#{__method__}] action of article controller..."
+
     @article = Article.find(params[:id])
   end
 
   def create
+    logger.info "---> entering [#{__method__}] action of article controller..."
+
    @article = Article.new(article_params)
 
    if @article.save
+    logger.info "---> entering [#{__method__}] action of article controller..."
+
     redirect_to @article
    else
     render 'new' # 这里用render（而不是redirect_to），会把原来的数据传回给new这个页面
@@ -28,10 +36,14 @@ class ArticlesController < ApplicationController
   end
 
   def show
+    logger.info "---> entering [#{__method__}] action of article controller..."
+
     @article = Article.find(params[:id])
   end
 
   def update
+    logger.info "---> entering [#{__method__}] action of article controller..."
+
     @article = Article.find(params[:id])
 
     if @article.update(article_params)
