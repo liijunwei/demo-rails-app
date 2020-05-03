d4686f2d16012a78e01419b9e0671ca29c35e2cd
lijunwei <lijunwei@udesk.cn>
2020-05-03 09:51:13 +0800

    test on gem holiday

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,12 @@
 class WelcomeController < ApplicationController
   # 展示首页
   def index
+
+    puts "hh"
+    byebug
+    Holidays.on(Date.civil(2008, 4, 25), :au)
+
+
+    render json: {"code": 200}
   end
 end
