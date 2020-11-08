df55fb6c3f2af71e77681e799efb90b57549cd07
lijunwei <lijunwei@udesk.cn>
2020-11-08 14:44:59 +0800

    Fix.

--- app/controllers/lee_controller.rb
+++ app/controllers/lee_controller.rb
@@ -10,11 +10,18 @@ class LeeController < ApplicationController
     # byebug
     # puts
     pass = "ljw"
-    if my_params[:key_foo].include?(pass)
-      redirect_to lee_index_path, notice: 'Pass token verified...'
+    # if my_params[:key_foo].include?(pass)
+    #   redirect_to lee_index_path, notice: 'Pass token verified...'
+    # else
+    #   render json: {mesage: "your key_foo do not have a #{pass}"}
+    # end
+    @article = Article.new(title: my_params[:key_foo],text: my_params[:value_foo])
+    if @article.save
+      redirect_to lee_index_path, notice: 'You Just create an article record using another controller...'
     else
-      render json: {mesage: "your key_foo do not have a #{pass}"}
+      render 'new'
     end
+
   end
 
   private
